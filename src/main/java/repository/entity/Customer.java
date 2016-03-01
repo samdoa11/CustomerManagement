0c6e1e21d5a5506350977e11c10ecd9bba8bc80b
goevea11 <goevea11@htlkaindorf.at>
2016-03-01 13:00:15 +0100

    repository

--- src/main/java/repository/entity/Customer.java
+++ src/main/java/repository/entity/Customer.java
@@ -1,7 +1,23 @@
 package repository.entity;
 
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+
 /**
  * Created by Veronika on 01.03.2016.
  */
-public class Customer {
+@Entity
+@Table
+@XStreamAlias("customer")
+public class Customer implements Serializable{
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
 }
