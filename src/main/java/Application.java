cd4b39135ee45319295527dd7f74972992a2ebcd
samdoa11 <samdoa11@htlkaindorf.at>
2016-03-01 12:57:37 +0100

    Grundstruktur

---
+++ src/main/java/Application.java
@@ -0,0 +1,13 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Dominik on 01.03.2016.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
