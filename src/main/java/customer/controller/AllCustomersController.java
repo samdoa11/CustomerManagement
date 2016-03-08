05bd2256f549ababb94dae15aad36886eda0d631
goevea11 <goevea11@htlkaindorf.at>
2016-03-08 13:02:50 +0100

    Update Customers

--- src/main/java/customer/controller/AllCustomersController.java
+++ src/main/java/customer/controller/AllCustomersController.java
@@ -3,18 +3,16 @@ package customer.controller;
 import customer.repository.entity.Customer;
 import customer.services.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by Dominik on 01.03.2016.
  */
-@Controller
+@RestController
 public class AllCustomersController {
-
-
 
 
     @Autowired
@@ -28,11 +26,17 @@ public class AllCustomersController {
     }
 
 
-
     @RequestMapping(value="/customer", method=RequestMethod.DELETE)
-    public void readCustomer(@RequestBody Customer customer)
+    public void deleteCustomer(@RequestBody Customer customer)
     {
         customerService.deleteCustomer(customer);
+    }
+
+    @RequestMapping(value="/customer", method=RequestMethod.PUT)
+    public Long readCustomer(@RequestBody Customer c)
+    {
+        customerService.updateCustomer(c);
+        return c.getId();
     }
 
 
