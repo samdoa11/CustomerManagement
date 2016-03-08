d97c3652b988400e43ef7fadd5905c5c00dfa266
goevea11 <goevea11@htlkaindorf.at>
2016-03-08 12:49:01 +0100

    Customer Controller read all and delete

--- src/main/java/customer/controller/AllCustomersController.java
+++ src/main/java/customer/controller/AllCustomersController.java
@@ -1,24 +1,39 @@
 package customer.controller;
 
+import customer.repository.entity.Customer;
 import customer.services.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 /**
  * Created by Dominik on 01.03.2016.
  */
 @Controller
 public class AllCustomersController {
 
+
+
+
     @Autowired
     private CustomerService customerService;
 
-    @RequestMapping(value="/Customers")
-    public String index(Model model){
-        //model.addAttribute("customers",customerService.getAllCustomers());
-        return "CustomerConfiguration";
+    @RequestMapping(value="/customer", method= RequestMethod.POST)
+    public Iterable<Customer> readAll(){
+
+        return  customerService.getAllCustomers();
 
     }
+
+
+
+    @RequestMapping(value="/customer", method=RequestMethod.DELETE)
+    public void readCustomer(@RequestBody Customer customer)
+    {
+        customerService.deleteCustomer(customer);
+    }
+
 
 }
