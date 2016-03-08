4285db9a659a9e8f752b185467d8b6337c392efd
samdoa11 <samdoa11@htlkaindorf.at>
2016-03-08 11:53:24 +0100

    Customer with entities

--- src/main/java/repository/entity/Customer.java
+++ src/main/java/repository/entity/Customer.java
@@ -2,11 +2,9 @@ package repository.entity;
 
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.io.Serializable;
+import java.util.Date;
 
 /**
  * Created by Veronika on 01.03.2016.
@@ -19,5 +17,99 @@ public class Customer implements Serializable{
     @GeneratedValue
     private Long id;
 
-    private String name;
+    @Column
+    private String fistname;
+
+    @Column
+    private String lastname;
+
+    @Column
+    private String username;
+
+    @Column
+    private String strasse;
+
+    @Column
+    private int plz;
+
+    @Column
+    private String ort;
+
+    @Column
+    private Date birthdate;
+
+    @Column
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFistname() {
+        return fistname;
+    }
+
+    public void setFistname(String fistname) {
+        this.fistname = fistname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getStrasse() {
+        return strasse;
+    }
+
+    public void setStrasse(String strasse) {
+        this.strasse = strasse;
+    }
+
+    public int getPlz() {
+        return plz;
+    }
+
+    public void setPlz(int plz) {
+        this.plz = plz;
+    }
+
+    public String getOrt() {
+        return ort;
+    }
+
+    public void setOrt(String ort) {
+        this.ort = ort;
+    }
+
+    public Date getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(Date birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
