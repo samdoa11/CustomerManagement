76eaebf221a2079eb8b8381ce396fb490ee7e496
samdoa11 <samdoa11@htlkaindorf.at>
2016-04-05 12:10:33 +0200

    tests

--- src/main/test/controller/ProductControllerIntegrationTest.java
+++ src/main/test/controller/ProductControllerIntegrationTest.java
@@ -1,6 +1,6 @@
 package controller;
 
-import com.infonova.education.microservice.repository.entity.Product;
+import customer.repository.entity.Customer;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -34,34 +34,8 @@ public class ProductControllerIntegrationTest extends AbstractControllerIntegrat
     @SuppressWarnings("unchecked")
     public void testFindAllProducts() throws Exception {
         String url = String.format("http://localhost:%s/products", port);
-        List<Product> products = (List<Product>) restTemplate.getForObject(url, List.class);
+        List<Customer> products = (List<Customer>) restTemplate.getForObject(url, List.class);
 
         Assert.assertEquals(0, products.size());
     }
-
-    @Test
-    @SuppressWarnings("unchecked")
-    public void testAddProduct() {
-
-        // save a new product
-        // ******************
-
-        Product product = new Product();
-        product.setName("Test");
-        product.setId(1L);
-
-        String url = String.format("http://localhost:%s/products", port);
-
-        RequestCallback callback = prepareHttpRequest(product);
-        ResponseExtractor<?> extractor = verifyResponse(HttpStatus.NO_CONTENT);
-
-        restTemplate.execute(url, HttpMethod.PUT, callback, extractor);
-
-        // verify products count
-        // *********************
-        List<Product> products = (List<Product>) restTemplate.getForObject(url, List.class);
-        Assert.assertEquals(1, products.size());
-
-    }
-
 }
