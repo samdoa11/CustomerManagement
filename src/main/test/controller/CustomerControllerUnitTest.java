db74c8b5aa1040bcef57f221b9746e0b7e1001e4
samdoa11 <samdoa11@htlkaindorf.at>
2016-04-05 12:58:18 +0200

    demo configurer angepasst

--- src/main/test/controller/CustomerControllerUnitTest.java
+++ src/main/test/controller/CustomerControllerUnitTest.java
@@ -2,6 +2,7 @@ package controller;
 
 import customer.Application;
 import customer.controller.AllCustomersController;
+import customer.repository.entity.Customer;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,8 +15,11 @@ import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
 
+import java.util.Date;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
@@ -39,12 +43,30 @@ public class CustomerControllerUnitTest {
     }
 
     @Test
-    public void testFindAllCustomersWithoutException() throws Exception {
+    public void testFindAllCustomersWithoutExtention() throws Exception {
         this.mockMvc.perform(post("/customer"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType("application/json;charset=UTF-8"))
                 .andExpect(jsonPath("$[0].lastname").exists())
                 .andExpect(jsonPath("$[1].lastname").exists());
+    }
+
+    @Test
+    public void testPutCustomerWithoutExtention() throws Exception {
+        Customer customer = new Customer();
+        customer.setBirthdate(new Date(2010, 01,01));
+        customer.setFistname("Vroni");
+        customer.setLastname("Geo");
+        customer.setOrt("sdf");
+        customer.setPassword("sdf");
+        customer.setPlz(11111);
+        customer.setStrasse("sdf");
+        customer.setUsername("vroni");
+
+        this.mockMvc.perform(put("/customer", customer))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json;charset=UTF-8"))
+                .andExpect(jsonPath("$[0]").exists());
     }
 
 }
