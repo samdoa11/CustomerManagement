fa279f229739734c618d3d19391c4ae4af031cdf
samdoa11 <samdoa11@htlkaindorf.at>
2016-04-17 15:21:55 +0200

    integration test for put customer

--- src/main/test/controller/CustomerControllerIntegrationTest.java
+++ src/main/test/controller/CustomerControllerIntegrationTest.java
@@ -1,5 +1,6 @@
 package controller;
 
+import customer.CustomerBuilder;
 import customer.repository.entity.Customer;
 import org.junit.Assert;
 import org.junit.Before;
@@ -9,11 +10,15 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.TestRestTemplate;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.web.client.RequestCallback;
 import org.springframework.web.client.ResponseExtractor;
 import org.springframework.web.client.RestTemplate;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import java.net.URI;
 import java.util.List;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -43,13 +48,16 @@ public class CustomerControllerIntegrationTest extends AbstractControllerIntegra
     public void testGetAllCustomers() throws Exception {
         String url = String.format("http://localhost:%s/customer?customerId=1", port);
         List<Customer> customers = (List<Customer>) restTemplate.postForObject(url, null, List.class);
-        Assert.assertEquals(customers.size(), 2);
+        Assert.assertThat(customers.size(), equalTo(3));
     }
 
     @Test
     public void testPutNewCustomer() throws Exception {
         String url = String.format("http://localhost:%s/customer", port);
-        Customer newCustomer = new Customer();
-
+        Customer newCustomer = CustomerBuilder.createDummyCustomer();
+        newCustomer.setId(10L);
+        RequestEntity entity = new RequestEntity<Customer>(newCustomer, HttpMethod.PUT, new URI(url));
+        ResponseEntity<Long> erg = restTemplate.exchange(url, HttpMethod.PUT, entity, Long.class);
+        Assert.assertThat(erg.getBody(), equalTo(10L));
     }
 }
